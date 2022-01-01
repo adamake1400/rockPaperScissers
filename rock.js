e8409151a9b5eec303eb19b6fca84a554deb7dab
adamake1400 <adamake1400@gmail.com>
2022-01-01 04:52:20 -0800

    add game, playerplay functions

--- rock.js
+++ rock.js
@@ -1,7 +1,42 @@
 
-playerSelection();
-computerPlay();
 
+
+
+//playRound(computerSelectionValue,playerSelectionValue);
+game();
+function game(){
+    let countOfPlayerWinnig=0;
+    let countOfComputerWinnig=0;
+    for(i=1;i<=5;i++){
+        let playerSelection=prompt("enter your selection:1-rock  2-papar 3-scissers");
+        let theWinner=playRound(playerSelection);
+        if(theWinner==="player")
+            countOfPlayerWinnig++;
+        if(theWinner==="computer")
+            countOfComputerWinnig++;
+    }
+    console.log("************************")
+    if (countOfPlayerWinnig>countOfComputerWinnig)
+        console.log("you are final winner");
+        else 
+            console.log("computer is final winner");
+    console.log("the number of computer winnig:"+countOfComputerWinnig , "the number of player winnig:"+countOfPlayerWinnig);
+}
+function playRound(playerSelection){
+    let playerSelectionValue=playerPlay(playerSelection);
+    let computerSelection=computerPlay();
+    console.log(computerSelection,playerSelectionValue);
+ if ((computerSelection=="scissors"&&playerSelectionValue=="rock")||(computerSelection=="rock"&&playerSelectionValue=="paper")||(computerSelection=="paper"&&playerSelectionValue=="scissors")){
+ console.log("------------player is Winner => "+playerSelectionValue+" beat "+computerSelection);
+ return "player";
+ }
+ else if((playerSelectionValue=="scissors"&& computerSelection=="rock")||(playerSelectionValue=="rock"&&computerSelection=="paper")||(playerSelectionValue=="paper"&&computerSelection=="scissors")){
+console.log("+++++++++++++computer is Winner => "+computerSelection+" beat "+playerSelectionValue);
+return "computer"; 
+}
+        else console.log("both are the same try again");
+ console.log("---------------------");
+}
 function computerPlay(){
     let computerSelection=randomNumber().toString();
     switch(computerSelection){
@@ -21,14 +56,14 @@ function computerPlay(){
             alert("somthing is wrong");
             
     }
-    
+    return computerSelection;
 }
 function randomNumber(){
     const r = Math.random()*3 + 1;
     return Math.floor(r)
 }
-function playerSelection(){
-    let playerSelection = prompt("Select your choice:1-Rock 2-Paper 3-Scissors")
+function playerPlay(playerSelection){
+   // let playerSelection = prompt("Select your choice:1-Rock 2-Paper 3-Scissors")
     switch(playerSelection){
         case "1":
             playerSelection="rock";
@@ -46,5 +81,6 @@ function playerSelection(){
             alert("Enter correct number(1,2,3)");
             
     }
+    return playerSelection;
 }
-function playRound(computerSelection,playerSelection)
+
